49cc8203037e55e2d3d87a4fbb8aa805653f857e
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-02-18 17:24:38 -0300

    Aula 01 - Introdução à Ruby

---
+++ ola_mundo.rb
@@ -0,0 +1,9 @@
+puts "Olá, Mundo!" #puts quebra a linha no final
+print "Olá, Mundo 2" #print mantém na mesma linha
+#Ruby executa o código na ordem em que foi escrito
+puts ""
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+
