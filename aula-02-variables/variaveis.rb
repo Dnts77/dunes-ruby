69b6b53e92bccda0421c4a4e682cca77987d0ad4
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-02-18 20:13:02 -0300

    Aula 02 - Variáveis

---
+++ aula-02-variables/variaveis.rb
@@ -0,0 +1,7 @@
+character_name = "Daniel" #variaveis com nomes compostos em Rubysão separadas por _ , diferentemente do Dart que se usa camelCase
+character_age = 16
+
+puts "Havia um homem chamado #{character_name}"
+puts "Ele tinha #{character_age} anos" #Em Dart, váriaveis são inseridas dentro do texto com $nomeVariavel ou ${nomeVariavel}, já em Ruby, usa-se #{nome_variavel}
+puts ("Ele gostava de se chamar " + character_name)
+puts "Mas ele não gostava de ter #{character_age} anos"
