53388e936f0036ba45bb7035ca423b96cec238ca
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-02-21 18:48:26 -0300

    Aula 17 - Loops For

---
+++ aula-17-for-loops/for.rb
@@ -0,0 +1,21 @@
+friends = ["Kevin", "Oscar", "Angela", "Andy", "Karen"]
+
+for friend in friends #Estrutura de um for
+  puts friend
+end
+
+friends.each do |friend| #Outra forma de mostrar todos os elementos de um array
+  puts friend
+end
+
+for index in 0..5 
+  puts index
+end
+
+6.times do |index|
+  puts index
+end
+
+=begin 
+Várias maneiras de usar loops for
+=end
