25482656534189e37e16e746f133b72ad332f13f
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-02-21 15:09:30 -0300

    Delete aula-010-methods directory

--- aula-010-methods/methods.rb
+++
@@ -1,9 +0,0 @@
-#Métodos/Funções
-
-def dizeroi(nome, idade)
-  
-  puts "Oi usuário #{nome}, você tem #{idade} anos!"
-
-end
-
-dizeroi("Daniel", 16) #As funções podem ser chamadas com () ex: dizeroi(), podem ser chamadas sem () e podem ser chamadas após print ou puts, ex: puts dizeroi() ou print dizeroi
