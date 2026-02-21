6b1b65e3a6dc5aff53faba035358c76661b3f49f
Daniel Dantas <daniel.souzadantas07@gmail.com>
2026-02-21 13:28:34 -0300

    Aula 07 - Calculadora Básica

---
+++ aula-07-basic-calculator/calculator.rb
@@ -0,0 +1,5 @@
+print "Digite um número:"
+num1 = gets.chomp.to_i #Conversão direto na variável
+print "Digite outro número:"
+num2 = gets.chomp.to_i
+print "A soma dos números é #{num1.to_i + num2.to_i}" #Strings convertidas pra ints
